5e00bdaad38ca01c85aa11fa688dc7221db72ed3
jerryfong16 <jerryfong16@outlook.com>
2024-09-19 21:16:04 -0400

    week1 HW

--- week1/main.go
+++ week1/main.go
@@ -1,5 +1,18 @@
 package main
 
 func main() {
+}
 
+func removeIndex[T any](s1 []T, i int) []T {
+	if i < 0 || i > len(s1) || len(s1) <= 1 {
+		return s1
+	}
+	s2 := make([]T, len(s1)-1)
+	for j := 0; j < len(s1); j++ {
+		if i == j {
+			continue
+		}
+		s2[j] = s1[j]
+	}
+	return s2
 }
