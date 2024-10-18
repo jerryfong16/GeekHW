83a0f8a35a6861b78db70b49d16a98945af10c3e
jerryfong16 <jerryfong16@outlook.com>
2024-10-18 12:05:52 -0400

    add week3 hw

---
+++ week3/week3-be/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"fzy.com/geek-hw-week3/repository/cache"
+	"github.com/redis/go-redis/v9"
+
+	"fzy.com/geek-hw-week3/config"
+	"fzy.com/geek-hw-week3/controller"
+	"fzy.com/geek-hw-week3/controller/middleware"
+	"fzy.com/geek-hw-week3/repository"
+	"fzy.com/geek-hw-week3/repository/dao"
+	"fzy.com/geek-hw-week3/service"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	db := initDB()
+	redisCache := initRedis()
+	server := initServer()
+	initWebControllers(db, redisCache, server)
+	server.Run(":8081")
+}
+
+func initDB() *gorm.DB {
+	db, err := gorm.Open(mysql.Open(config.Config.DB.DSN))
+	if err != nil {
+		panic("fail to connect database")
+	}
+
+	if err := dao.InitTables(db); err != nil {
+		panic("fail to init tables")
+	}
+
+	return db
+}
+
+func initRedis() redis.Cmdable {
+	return redis.NewClient(&redis.Options{
+		Addr: config.Config.Redis.Addr,
+	})
+}
+
+func initServer() *gin.Engine {
+	server := gin.Default()
+
+	// configure CORS
+	server.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		ExposeHeaders: []string{"x-jwt"},
+		AllowMethods:  []string{"OPTIONS", "POST", "DELETE", "PUT", "GET"},
+		MaxAge:        12 * time.Hour,
+	}))
+
+	// configure login session middleware
+	//server.Use(sessions.Sessions("sid", cookie.NewStore([]byte("geek_hw"))))
+	//loginMiddleware := &middleware.LoginMiddlewareBuilder{}
+	//server.Use(loginMiddleware.CheckLogin())
+
+	// configure login jwt middleware
+	loginJWTMiddleware := &middleware.LoginJWTMiddlewareBuilder{}
+	server.Use(loginJWTMiddleware.CheckLogin())
+
+	server.GET("/", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Welcome")
+	})
+
+	return server
+}
+
+func initWebControllers(db *gorm.DB, redisCache redis.Cmdable, server *gin.Engine) {
+	accountDAO := dao.NewAccountDAO(db)
+	accountCache := cache.NewAccountCache(redisCache)
+	accountRepository := repository.NewAccountRepository(accountDAO, accountCache)
+	accountService := service.NewAccountService(accountRepository)
+	accountController := controller.NewAccountController(accountService)
+	accountController.RegisterRoutes(server)
+}
