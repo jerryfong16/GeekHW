a95eaaff8277b091ed48921efc870e20759f5d5b
fongziyjun16 <fongziyjun16@hotmail.com>
2024-12-19 11:13:21 +0800

    add integration test

---
+++ week5-be/internal/integration_test/startup/wire.go
@@ -0,0 +1,35 @@
+//go:build wireinject
+
+package integration_test_startup
+
+import (
+	"geek-hw-week5/internal/repository"
+	"geek-hw-week5/internal/repository/cache"
+	"geek-hw-week5/internal/repository/dao"
+	"geek-hw-week5/internal/service"
+	"geek-hw-week5/internal/web"
+	"geek-hw-week5/ioc"
+	"github.com/gin-gonic/gin"
+	"github.com/google/wire"
+)
+
+func InitWebServer() *gin.Engine {
+	wire.Build(
+		ioc.InitDB, ioc.InitRedis,
+
+		dao.NewUserDAO,
+
+		cache.NewUserCache, cache.NewCodeCache,
+
+		repository.NewUserRepository, repository.NewCodeRepository,
+
+		ioc.InitLocalSMSService,
+		service.NewUserService, service.NewCodeService,
+
+		web.NewUserHandler,
+
+		ioc.InitGinMiddlewares,
+		ioc.InitWebServer,
+	)
+	return gin.Default()
+}
